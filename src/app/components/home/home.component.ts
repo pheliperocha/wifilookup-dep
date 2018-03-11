9d6c2d7996a048d12b3da482df32e5a3dc0e470f
Phelipe Rocha <phelipe.rocha@celsolisboa.edu.br>
2018-03-11 20:10:18 -0300

    Using node-nmap lib

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -14,7 +14,7 @@ export class HomeComponent implements OnInit {
   ngOnInit() {}
 
   test() {
-    this.electronService.mockScanNetwork();
+    this.electronService.scanNetwork();
   }
 
 }
--- src/app/providers/electron.service.ts
+++ src/app/providers/electron.service.ts
@@ -1,18 +1,16 @@
 import { Injectable } from '@angular/core';
 
-import { ipcRenderer } from 'electron';
+import { ipcRenderer, remote } from 'electron';
 import * as childProcess from 'child_process';
-import { ChildProcess } from 'child_process';
-import { Devices } from '../models/device';
 
 @Injectable()
 export class ElectronService {
 
   ipcRenderer: typeof ipcRenderer;
   childProcess: typeof childProcess;
+  remote: typeof remote;
   defaultIPGateway: string;
-  private nmap: ChildProcess;
-  public devices: Devices[] = [];
+  private nmap: any;
 
   public gatewayRegex = /(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])/g;
   public ipv4Regex = new RegExp(/([0-9]{1,3}\.){3}[0-9]{1,3}(([0-9]|[1-2][0-9]|3[0-2]))?/g);
@@ -23,8 +21,10 @@ export class ElectronService {
     if (this.isElectron()) {
       this.ipcRenderer = window.require('electron').ipcRenderer;
       this.childProcess = window.require('child_process');
+      this.remote = window.require('electron').remote;
+      this.nmap = this.remote.require('node-nmap');
+
       this.getDefaultIPGateway();
-      // this.scanNetwork();
     }
   }
 
@@ -40,7 +40,23 @@ export class ElectronService {
   }
 
   public scanNetwork() {
-    this.nmap = this.childProcess.spawn('nmap', ['-sP', this.defaultIPGateway + '/24']);
+    console.log('Starting Scan');
+
+    var quickscan = new this.nmap.QuickScan(this.defaultIPGateway + '/24');
+
+    quickscan.on('complete', function(data) {
+      console.log(data);
+    });
+
+    quickscan.on('error', function(error) {
+      console.log(error);
+    });
+
+    quickscan.startScan();
+  }
+
+  public scanDevice(ip: string) {
+    this.nmap = this.childProcess.spawn('nmap', ['-sP', ip]);
     this.nmap.stdout.setEncoding('utf8');
 
     this.nmap.stdout.on('data', function (data) {
@@ -57,55 +73,6 @@ export class ElectronService {
       console.log('Error =======');
       console.log(data);
     });
-  }
-
-  public mockScanNetwork() {
-    const result = "Nmap scan report for 192.168.1.1\n" +
-      "Host is up (0.0020s latency).\n" +
-      "MAC Address: E4:18:6B:12:90:EE (ZyXEL Communications)\n" +
-      "Nmap scan report for 192.168.1.3\n" +
-      "Host is up (1.3s latency).\n" +
-      "MAC Address: CC:C3:EA:4E:EC:E6 (Motorola Mobility, a Lenovo Company)\n" +
-      "Nmap scan report for 192.168.1.5\n" +
-      "Host is up (0.077s latency).\n" +
-      "MAC Address: 6C:AD:F8:54:29:52 (AzureWave Technology)\n" +
-      "Nmap scan report for 192.168.1.8\n" +
-      "Host is up (0.25s latency).\n" +
-      "MAC Address: D4:63:C6:51:C2:C6 (Motorola Mobility, a Lenovo Company)\n" +
-      "Nmap scan report for 192.168.1.10\n" +
-      "Host is up (0.23s latency).\n" +
-      "MAC Address: 74:E5:43:DB:B5:6F (Liteon Technology)\n" +
-      "Nmap scan report for 192.168.1.12\n" +
-      "Host is up (0.33s latency).\n" +
-      "MAC Address: 90:FB:A6:75:A3:33 (Hon Hai Precision Ind.)\n" +
-      "Nmap scan report for 192.168.1.13\n" +
-      "Host is up (0.40s latency).\n" +
-      "MAC Address: 54:27:1E:F9:00:E0 (AzureWave Technology)\n" +
-      "Nmap scan report for 192.168.1.148\n" +
-      "Host is up (1.5s latency).\n" +
-      "MAC Address: EE:08:6B:F8:F7:70 (Unknown)\n" +
-      "Nmap scan report for 192.168.1.4\n" +
-      "Host is up.\n" +
-      "Nmap done: 256 IP addresses (9 hosts up) scanned in 34.77 seconds";
-
-    let hosts = result.match(this.ipv4Regex);
-    let mac = result.match(this.macRegex);
-    let name = result.match(this.deviceNameRegex);
-
-    hosts.forEach((value, index) => {
-      this.devices.push({
-        ip: value,
-        mac: mac[index],
-        name: name[index]
-      })
-    });
-
-    console.log(result);
-    console.log(this.devices);
-  }
-
-  public scanDevice() {
-
   }
 
 }
